5db768aef3de8b49821805beed80fe7112c7dd59
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-07-18 20:19:01 -0500

    committing so I can switch to my laptop and watch the Twins!

--- public/scripts/controllers/landing.controller.js
+++ public/scripts/controllers/landing.controller.js
@@ -6,17 +6,4 @@ function LandingController() {
 
   const ctrl = this;
 
-  const pageContainer = document.querySelector('#landing-page');
-  let windowSize = {width: window.innerWidth, height: window.innerHeight};
-
-  ctrl.setWidth = function(e) {
-    console.log(windowSize);
-    console.log(pageContainer);
-    console.log(e);
-  }
-
-  ctrl.setWidth();
-
-  pageContainter.addEventListener('resize', ctrl.getWidth);
-
 }
